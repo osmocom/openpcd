3b8d883129391de3514a1156afe820b31b3ba284
(no author) <(no author)@6dc7ffe9-61d6-0310-9af1-9938baff3ed1>
2006-07-25 07:26:19 +0000

    changed directory structure
    git-svn-id: https://svn.openpcd.org:2342/trunk@40 6dc7ffe9-61d6-0310-9af1-9938baff3ed1

---
+++ openpcd/firmware/include/board.h
@@ -0,0 +1,13 @@
+#ifndef __BOARD_H__
+#define __BOARD_H__
+
+#include <AT91SAM7.h>
+#include <lib_AT91SAM7.h>
+
+/*--------------*/
+/* Master Clock */
+/*--------------*/
+#define EXT_OSC		18432000	// External Crystal Oscillator
+#define MCK		47923200	// Resulting PLL CLock
+
+#endif/*__BOARD_H__*/
