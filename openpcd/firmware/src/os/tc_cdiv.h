ff32f694fb2a0e2087b82e7903752a615d81ec44
(no author) <(no author)@6dc7ffe9-61d6-0310-9af1-9938baff3ed1>
2006-09-09 10:45:06 +0000

    TC_CDIV (carrier divisor) is part of both PICC and PCD, move it to 'os'
    git-svn-id: https://svn.openpcd.org:2342/trunk@150 6dc7ffe9-61d6-0310-9af1-9938baff3ed1

---
+++ openpcd/firmware/src/os/tc_cdiv.h
@@ -0,0 +1,23 @@
+#ifndef _TC_CDIV_H
+#define _TC_CDIV_H
+
+#include <sys/types.h>
+
+extern void tc_cdiv_phase_add(int16_t inc);
+extern void tc_cdiv_set_divider(u_int16_t div);
+
+static inline void tc_cdiv_phase_inc(void)
+{
+       tc_cdiv_phase_add(1);
+}
+
+static inline void tc_cdiv_phase_dec(void)
+{
+       tc_cdiv_phase_add(-1);
+}
+
+extern void tc_cdiv_print(void);
+extern void tc_cdiv_init(void);
+extern void tc_cdiv_fini(void);
+
+#endif
