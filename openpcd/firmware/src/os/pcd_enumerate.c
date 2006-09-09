1965e3ced86c090f5996fd38810efe7750c66f48
(no author) <(no author)@6dc7ffe9-61d6-0310-9af1-9938baff3ed1>
2006-09-09 03:10:12 +0000

    update dfu_api to new address
    git-svn-id: https://svn.openpcd.org:2342/trunk@144 6dc7ffe9-61d6-0310-9af1-9938baff3ed1

--- openpcd/firmware/src/os/pcd_enumerate.c
+++ openpcd/firmware/src/os/pcd_enumerate.c
@@ -39,7 +39,7 @@
 #define AT91C_EP_INT  3
 
 #ifdef CONFIG_DFU
-#define DFU_API_LOCATION	((const struct dfuapi *) 0x1f00)
+#define DFU_API_LOCATION	((const struct dfuapi *) 0x00102100)
 static const struct dfuapi *dfu = DFU_API_LOCATION;
 #define udp_ep0_send_data	dfu->ep0_send_data
 #define udp_ep0_send_zlp	dfu->ep0_send_zlp
