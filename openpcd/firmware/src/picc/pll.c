9b752b403f60b4d64e4165d20877241abcbfe2d4
(no author) <(no author)@6dc7ffe9-61d6-0310-9af1-9938baff3ed1>
2006-09-10 16:59:51 +0000

    - Add notification in case PLL PIO status changes (serial + LED1)
    git-svn-id: https://svn.openpcd.org:2342/trunk@177 6dc7ffe9-61d6-0310-9af1-9938baff3ed1

--- openpcd/firmware/src/picc/pll.c
+++ openpcd/firmware/src/picc/pll.c
@@ -1,6 +1,8 @@
 
 #include <sys/types.h>
 #include <lib_AT91SAM7.h>
+#include <os/pio_irq.h>
+#include <os/dbgu.h>
 #include "../openpcd.h"
 
 void pll_inhibit(int inhibit)
@@ -16,9 +18,23 @@ int pll_is_locked(void)
 	return AT91F_PIO_IsInputSet(AT91C_BASE_PIOA, OPENPICC_PIO_PLL_LOCK);
 }
 
+static void pll_lock_change_cb(u_int32_t pio)
+{
+	DEBUGPCRF("PLL LOCK: %d", pll_is_locked());
+#if 1
+	if (pll_is_locked())
+		led_switch(1, 1);
+	else
+		led_switch(1, 0);
+#endif
+}
+
 void pll_init(void)
 {
 	AT91F_PIO_CfgOutput(AT91C_BASE_PIOA, OPENPICC_PIO_PLL_INHIBIT);
 	AT91F_PIO_CfgInput(AT91C_BASE_PIOA, OPENPICC_PIO_PLL_LOCK);
 	pll_inhibit(0);
+
+	pio_irq_register(OPENPICC_PIO_PLL_LOCK, &pll_lock_change_cb);
+	pio_irq_enable(OPENPICC_PIO_PLL_LOCK);
 }
