e4e1f954962e3388244d0859e886728439ddb6b2
henryk <henryk@6dc7ffe9-61d6-0310-9af1-9938baff3ed1>
2009-10-10 14:29:22 +0000

    Refactor the sniffer code to be more useful externally
    git-svn-id: https://svn.openpcd.org:2342/branches/legic@494 6dc7ffe9-61d6-0310-9af1-9938baff3ed1

--- openpicc/application/tc_sniffer.c
+++ openpicc/application/tc_sniffer.c
@@ -42,34 +42,27 @@
 #include "cmd.h"
 #include "pio_irq.h"
 
+#define WAIT_TICKS (20*portTICK_RATE_MS)
 /* Problem: We want to receive data from the FIQ without locking (the FIQ must not be blocked ever)
  * Strategy: Double buffering.
  */
+static tc_fiq_buffer_t fiq_buffers[2];
 
-#define BUFSIZE 1024
-#define WAIT_TICKS (20*portTICK_RATE_MS)
-typedef struct {
-	u_int32_t count;
-	u_int32_t data[BUFSIZE];
-} fiq_buffer_t;
-fiq_buffer_t fiq_buffers[2];
-
-fiq_buffer_t *tc_sniffer_next_buffer_for_fiq = 0;
+tc_fiq_buffer_t *tc_sniffer_next_buffer_for_fiq = 0;
 
 portBASE_TYPE currently_sniffing = 0;
 enum { NONE, REQUEST_START, REQUEST_STOP } request_change = NONE;
 
 #define MIN(a, b) ((a)>(b)?(b):(a))
-void flush_buffer(fiq_buffer_t *buffer)
+static void flush_buffer(tc_fiq_buffer_t *buffer)
 {
 	/* Write all data from the given buffer out, then zero the count */
 	if(buffer->count > 0) {
-		vUSBSendBuffer_blocking((unsigned char*)(&(buffer->data[0])), 0, MIN(buffer->count,BUFSIZE)*4, WAIT_TICKS);
-		if(buffer->count >= BUFSIZE)
+		vUSBSendBuffer_blocking((unsigned char*)(&(buffer->data[0])), 0, MIN(buffer->count,TC_FIQ_BUFSIZE)*4, WAIT_TICKS);
+		if(buffer->count >= TC_FIQ_BUFSIZE)
 			vUSBSendBuffer_blocking((unsigned char*)"////", 0, 4, WAIT_TICKS);
 		else
 			vUSBSendBuffer_blocking((unsigned char*)"____", 0, 4, WAIT_TICKS);
-		buffer->count = 0;
 	}
 }
 
@@ -81,10 +74,8 @@ void start_stop_sniffing(void)
 		request_change = REQUEST_START;
 }
 
-void tc_sniffer (void *pvParameters)
+void tc_fiq_setup(void)
 {
-	(void)pvParameters;
-	
 	/* Disable load modulation circuitry
 	 * (Must be done explicitly, because the default state is pull-up high, leading
 	 * to a constant modulation output which prevents reception. I've been bitten by
@@ -100,31 +91,55 @@ void tc_sniffer (void *pvParameters)
 	
 	memset(fiq_buffers, 0, sizeof(fiq_buffers));
 	
-	/* Wait for the USB and CMD threads to start up */
-	vTaskDelay(1000 * portTICK_RATE_MS);
-	
 	// The change interrupt is going to be handled by the FIQ 
 	AT91F_PIO_CfgInput(AT91C_BASE_PIOA, OPENPICC_SSC_DATA);
 	pio_irq_enable(OPENPICC_SSC_DATA);
+
+}
+
+void tc_fiq_start(void)
+{
 	
-	int current = 0;
+}
+
+static int current = 0;
+void tc_fiq_stop(void)
+{
+	tc_sniffer_next_buffer_for_fiq = 0;
+	memset(fiq_buffers, 0, sizeof(fiq_buffers));
+	current = 0;
+}
+
+void tc_fiq_process(tc_fiq_buffer_handler_t *handler)
+{
+	int next = (current+1)%(sizeof(fiq_buffers)/sizeof(fiq_buffers[0]));
+	handler( &fiq_buffers[next] );
+	fiq_buffers[next].count = 0;
+	/* The buffer designated by next is now empty, give it to the fiq,
+	 * we'll just guess that this write is atomic */ 
+	tc_sniffer_next_buffer_for_fiq = &fiq_buffers[current=next];
+}
+
+void tc_sniffer (void *pvParameters)
+{
+	(void)pvParameters;
+	
+	tc_fiq_setup();
+	
+	/* Wait for the USB and CMD threads to start up */
+	vTaskDelay(1000 * portTICK_RATE_MS);
+	
 	while(1) {
 		/* Main loop of the sniffer */
 		
 		if(currently_sniffing) {
-			int next = (current+1)%(sizeof(fiq_buffers)/sizeof(fiq_buffers[0]));
-			flush_buffer( &fiq_buffers[next] );
-			/* The buffer designated by next is now empty, give it to the fiq,
-			 * we'll just guess that this write is atomic */ 
-			tc_sniffer_next_buffer_for_fiq = &fiq_buffers[current=next];
-
+			tc_fiq_process(flush_buffer);
+			
 			if(request_change == REQUEST_STOP) {
 				currently_sniffing = 0;
 				request_change = NONE;
 				
-				tc_sniffer_next_buffer_for_fiq = 0;
-				memset(fiq_buffers, 0, sizeof(fiq_buffers));
-				current = 0;
+				tc_fiq_stop();
 				vUSBSendBuffer_blocking((unsigned char *)"----", 0, 4, WAIT_TICKS);
 				usb_print_set_force_silence(0);
 			} else vTaskDelay(2* portTICK_RATE_MS);
@@ -135,6 +150,7 @@ void tc_sniffer (void *pvParameters)
 				// Prevent usb_print code from barging in
 				usb_print_set_force_silence(1);
 				vUSBSendBuffer_blocking((unsigned char *)"----", 0, 4, WAIT_TICKS);
+				tc_fiq_start();
 				currently_sniffing = 1;
 				request_change = NONE;
 			} else vTaskDelay(100 * portTICK_RATE_MS);
--- openpicc/application/tc_sniffer.h
+++ openpicc/application/tc_sniffer.h
@@ -1,7 +1,18 @@
 #ifndef TC_SNIFFER_H_
 #define TC_SNIFFER_H_
 
+#define TC_FIQ_BUFSIZE 1024
+typedef struct {
+	u_int32_t count;
+	u_int32_t data[TC_FIQ_BUFSIZE];
+} tc_fiq_buffer_t;
+typedef void tc_fiq_buffer_handler_t(tc_fiq_buffer_t *buffer);
+
 extern void tc_sniffer (void *pvParameters);
+extern void tc_fiq_setup(void);
+extern void tc_fiq_start(void);
+extern void tc_fiq_stop(void);
+extern void tc_fiq_process(tc_fiq_buffer_handler_t *handler);
 extern void start_stop_sniffing (void);
 
 #endif /*TC_SNIFFER_H_*/
