9bd1b003a63d61e91db3827c2b66290abc1adf13
Min Xu <min.xu@min-info.net>
2014-10-25 20:54:09 +0200

    iso7816_uart.h: add missing #ifdef/endif to avoid multiple inclusion

--- firmware/src/simtrace/iso7816_uart.h
+++ firmware/src/simtrace/iso7816_uart.h
@@ -1,3 +1,5 @@
+#ifndef SIMTRACE_ISO7816_UART_H
+#define SIMTRACE_ISO7816_UART_H
 
 struct simtrace_stats *iso_uart_stats_get(void);
 void iso_uart_stats_dump(void);
@@ -7,3 +9,5 @@ void iso_uart_rx_mode(void);
 void iso_uart_clk_master(unsigned int master);
 void iso_uart_init(void);
 void iso_uart_report_overrun(void);
+
+#endif
